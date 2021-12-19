15e9263ae3370abbb0b1f409bacbe940a79d30ad
rudek11 <rudek11@interia.pl>
2021-12-19 22:44:41 +0100

    done

--- src/main/java/pl/coderslab/apierwszyprogram/Main05.java
+++ src/main/java/pl/coderslab/apierwszyprogram/Main05.java
@@ -3,6 +3,6 @@ package pl.coderslab.apierwszyprogram;
 public class Main05 {
 
 	public static void main(String[] args) {
-//		System.out.println("Hello world")
+		 System.out.println("Hello world");
 	}
 }
